28fd620650fafbf41e28449e6968575eec534883
TruongVanPhon <truongvanphon020297@gmail.com>
2022-03-09 16:27:16 +0700

    add folder Key_Press 16h27

---
+++ For_Lop/Demo.js
@@ -0,0 +1,4 @@
+let Text ="";
+for(let i = 0; i < 5 ; i ++){
+    Text
+}
---
+++ Key_Press/Demo.js
@@ -0,0 +1,36 @@
+function hTop(){
+    let Img_value = document.getElementById("img");
+    Img_value.style.top = parseInt(Img_value.style.top) - 5 + "px";
+}
+function hBottom(){
+    let Img_value = document.getElementById("img");
+    Img_value.style.top = parseInt(Img_value.style.top) + 5 + "px";
+}
+function hLeft(){
+    let Img_value = document.getElementById("img");
+    Img_value.style.left = parseInt(Img_value.style.left) - 5 + "px";
+}
+function hRight(){
+    let Img_value = document.getElementById("img");
+    Img_value.style.left = parseInt(Img_value.style.left) + 5 + "px";
+}
+
+function Move_Key(Event){
+    switch (Event.keyCode) {
+        case 37:
+            hLeft();
+            break;
+        case 39:
+            hRight();
+            break;
+        case 38:
+            hTop();
+            break;
+        case 40:
+            hBottom();
+            break;
+    }
+}
+function Do_Ready(){
+    window.addEventListener('keydown', Move_Key);
+}
